1496528a494c860cb48b9082611f114c3aff55e0
tranggiang <47005726+tranggiang@users.noreply.github.com>
2023-08-14 19:27:33 +0700

    update query

--- src/main/java/com/wego/test/carpark/repo/CarParkRepo.java
+++ src/main/java/com/wego/test/carpark/repo/CarParkRepo.java
@@ -16,10 +16,10 @@ public interface CarParkRepo extends JpaRepository<CarPark, String> {
     @Query(value = " SELECT  cp.address as address , cp.car_park_number as carParkNumber,cp.point as point,SUM(ci.total_lots) as total_lots,SUM(ci.lots_available) as available_lots ," +
             " ST_DISTANCE(point,ST_GEOMFROMTEXT(:pointParam, 4326)) / 1000 AS km  FROM car_park cp " +
             "JOIN  carpark_info  ci " +
-            "WHERE cp.car_park_number = ci.car_park_id GROUP BY ci.car_park_id  ORDER BY km  ",
-            countQuery = "SELECT count(cp.car_park_number),ST_DISTANCE(point,ST_GEOMFROMTEXT(:pointParam, 4326)) / 1000 as km FROM car_park cp, carpark_info ci  " +
-                    " WHERE cp.car_park_number = ci.car_park_number GROUP BY cp.car_park_number " +
-                    " ORDER BY km HAVING  lots_available > 0", nativeQuery = true)
+            "WHERE cp.car_park_number = ci.car_park_id GROUP BY ci.car_park_id  HAVING available_lots > 0  ORDER BY km ",
+            countQuery = "SELECT count(cp.car_park_number),SUM(ci.lots_available) as available_lots,ST_DISTANCE(point,ST_GEOMFROMTEXT(:pointParam, 4326)) / 1000 as km FROM car_park cp, carpark_info ci  " +
+                    " WHERE cp.car_park_number = ci.car_park_id GROUP BY cp.car_park_number " +
+                    " HAVING available_lots > 0 ORDER BY km ", nativeQuery = true)
     List<CarParkReport> findNearestParking(@Param("pointParam") String p, Pageable pageable);
 
 
