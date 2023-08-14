971d3d9a925014fcf4f04d6143b0e8c1b75bc89f
tranggiang <47005726+tranggiang@users.noreply.github.com>
2023-08-14 17:21:54 +0700

    convert to constant

--- src/main/java/com/wego/test/carpark/service/imp/CarParkLoader.java
+++ src/main/java/com/wego/test/carpark/service/imp/CarParkLoader.java
@@ -15,6 +15,20 @@ import java.util.List;
 @Component
 public class CarParkLoader {
 
+    public static final String CAR_PARK_NO = "car_park_no";
+    public static final String ADDRESS = "address";
+    public static final String X_COORD = "x_coord";
+    public static final String Y_COORD = "y_coord";
+    public static final String CAR_PARK_TYPE = "car_park_type";
+    public static final String TYPE_OF_PARKING_SYSTEM = "type_of_parking_system";
+    public static final String SHORT_TERM_PARKING = "short_term_parking";
+    public static final String FREE_PARKING = "free_parking";
+    public static final String NIGHT_PARKING = "night_parking";
+    public static final String CAR_PARK_DECKS = "car_park_decks";
+    public static final String GANTRY_HEIGHT = "gantry_height";
+    public static final String CAR_PARK_BASEMENT = "car_park_basement";
+
+
     public static List<CarPark> loadCarPark() throws FileNotFoundException {
 
         File file = ResourceUtils.getFile("classpath:HDBCarparkInformation.csv");
@@ -27,13 +41,13 @@ public class CarParkLoader {
             List<CarPark> carParks = new ArrayList(csvRecords.size());
 
             for (CSVRecord csvRecord : csvRecords) {
-                CarPark carPark = new CarPark(csvRecord.get("car_park_no"), csvRecord.get("address"),
-                        Double.parseDouble(csvRecord.get("x_coord")), Double.parseDouble(csvRecord.get("y_coord")),
-                        csvRecord.get("car_park_type"), csvRecord.get("type_of_parking_system"),
-                        csvRecord.get("short_term_parking"), csvRecord.get("free_parking"),
-                        csvRecord.get("night_parking"), Integer.parseInt(csvRecord.get("car_park_decks")),
-                        Double.parseDouble(csvRecord.get("gantry_height")),
-                        Boolean.parseBoolean(csvRecord.get("car_park_basement")));
+                CarPark carPark = new CarPark(csvRecord.get(CAR_PARK_NO), csvRecord.get(ADDRESS),
+                        Double.parseDouble(csvRecord.get(X_COORD)), Double.parseDouble(csvRecord.get(Y_COORD)),
+                        csvRecord.get(CAR_PARK_TYPE), csvRecord.get(TYPE_OF_PARKING_SYSTEM),
+                        csvRecord.get(SHORT_TERM_PARKING), csvRecord.get(FREE_PARKING),
+                        csvRecord.get(NIGHT_PARKING), Integer.parseInt(csvRecord.get(CAR_PARK_DECKS)),
+                        Double.parseDouble(csvRecord.get(GANTRY_HEIGHT)),
+                        Boolean.parseBoolean(csvRecord.get(CAR_PARK_BASEMENT)));
 
                 carParks.add(carPark);
             }
